467a66400d14d76f51ffbc9dd434efb98fa70ec6
Rinardi Sarean <rinardi@accelbyte.net>
2024-06-27 20:43:17 +0700

    feat: update proto and implementation with binary record

--- src/AccelByte.PluginArch.CloudsaveValidator.Demo.Server/Services/CloudSaveValidatorService.cs
+++ src/AccelByte.PluginArch.CloudsaveValidator.Demo.Server/Services/CloudSaveValidatorService.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
 
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
@@ -20,7 +22,14 @@ namespace AccelByte.PluginArch.CloudsaveValidator.Demo.Server.Services
 {
     public class CloudSaveValidatorService : CloudsaveValidatorService.CloudsaveValidatorServiceBase
     {
-        private readonly ILogger<CloudSaveValidatorService> _Logger;
+        private readonly ILogger<CloudSaveValidatorService> _Logger;        
+
+        private static readonly HttpClient _HttpClient = new HttpClient(new HttpClientHandler()
+        {
+            AllowAutoRedirect = false
+        });
+
+        private long _MaxSizeEventBannerInKB = 100;
 
         public CloudSaveValidatorService(ILogger<CloudSaveValidatorService> logger)
         {
@@ -291,6 +300,162 @@ namespace AccelByte.PluginArch.CloudsaveValidator.Demo.Server.Services
         {
             BulkPlayerRecordValidationResult result = new BulkPlayerRecordValidationResult();
             foreach (var playerRecord in request.PlayerRecords)
+            {
+                result.ValidationResults.Add(new PlayerRecordValidationResult()
+                {
+                    IsSuccess = true,
+                    Key = playerRecord.Key,
+                    UserId = playerRecord.UserId
+                });
+            }
+
+            return Task.FromResult(result);
+        }
+
+        public override async Task<GameRecordValidationResult> BeforeWriteGameBinaryRecord(GameBinaryRecord request, ServerCallContext context)
+        {
+            if (request.Key.EndsWith("event_banner", StringComparison.OrdinalIgnoreCase)
+                && request.BinaryInfo != null)
+            {
+                HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Get, request.BinaryInfo.Url);
+                var resp = await _HttpClient.SendAsync(req);
+
+                var contentLengthInKB = resp.Content.Headers.ContentLength ?? 0 / 1000;
+                if (contentLengthInKB > _MaxSizeEventBannerInKB)
+                    throw new Exception($"maximum size for event banner is {_MaxSizeEventBannerInKB} kB");
+            }
+
+            return await Task.FromResult(new GameRecordValidationResult()
+            {
+                IsSuccess = true,
+                Key = request.Key
+            });
+        }
+
+        public override Task<GameRecordValidationResult> AfterReadGameBinaryRecord(GameBinaryRecord request, ServerCallContext context)
+        {
+            if (request.Key.EndsWith("daily_event_stage", StringComparison.OrdinalIgnoreCase)
+                && request.BinaryInfo != null)
+            {
+                try
+                {
+                    var binaryDto = request.BinaryInfo.UpdatedAt.ToDateTime();
+                    var currentDto = DateTime.UtcNow;
+
+                    if (binaryDto.ToShortDateString() != currentDto.ToShortDateString())
+                        throw new Exception($"Today's {request.Key} is not ready yet");
+                }
+                catch (Exception x)
+                {
+                    return Task.FromResult(new GameRecordValidationResult()
+                    {
+                        IsSuccess = false,
+                        Key = request.Key,
+                        Error = new Error()
+                        {
+                            ErrorCode = 1,
+                            ErrorMessage = x.Message
+                        }
+                    });
+                }
+            }
+
+            return Task.FromResult(new GameRecordValidationResult()
+            {
+                IsSuccess = true,
+                Key = request.Key
+            });
+        }
+
+        public override Task<BulkGameRecordValidationResult> AfterBulkReadGameBinaryRecord(BulkGameBinaryRecord request, ServerCallContext context)
+        {
+            BulkGameRecordValidationResult result = new BulkGameRecordValidationResult();
+            foreach (var record in request.GameBinaryRecords)
+            {
+                if (record.Key.EndsWith("daily_event_stage", StringComparison.OrdinalIgnoreCase)
+                    && record.BinaryInfo != null)
+                {
+                    try
+                    {
+                        var binaryDto = record.BinaryInfo.UpdatedAt.ToDateTime();
+                        var currentDto = DateTime.UtcNow;
+
+                        if (binaryDto.ToShortDateString() != currentDto.ToShortDateString())
+                            throw new Exception($"Today's {record.Key} is not ready yet");
+                    }
+                    catch (Exception x)
+                    {
+                        result.ValidationResults.Add(new GameRecordValidationResult()
+                        {
+                            IsSuccess = false,
+                            Key = record.Key,
+                            Error = new Error()
+                            {
+                                ErrorCode = 1,
+                                ErrorMessage = x.Message
+                            }
+                        });
+                    }
+                }
+
+                result.ValidationResults.Add(new GameRecordValidationResult()
+                {
+                    IsSuccess = true,
+                    Key = record.Key
+                });
+            }
+
+            return Task.FromResult(result);
+        }
+
+        public override Task<PlayerRecordValidationResult> BeforeWritePlayerBinaryRecord(PlayerBinaryRecord request, ServerCallContext context)
+        {
+            if (request.Key.EndsWith("id_card", StringComparison.OrdinalIgnoreCase)
+                && (request.BinaryInfo != null))
+            {
+                try
+                {
+                    if (request.BinaryInfo.Version > 1)
+                        throw new Exception("id card can only be created once");
+                }
+                catch (Exception x)
+                {
+                    return Task.FromResult(new PlayerRecordValidationResult()
+                    {
+                        IsSuccess = false,
+                        Key = request.Key,
+                        UserId = request.UserId,
+                        Error = new Error()
+                        {
+                            ErrorCode = 1,
+                            ErrorMessage = x.Message
+                        }
+                    });
+                }
+            }
+
+            return Task.FromResult(new PlayerRecordValidationResult()
+            {
+                IsSuccess = true,
+                Key = request.Key,
+                UserId = request.UserId
+            });
+        }
+
+        public override Task<PlayerRecordValidationResult> AfterReadPlayerBinaryRecord(PlayerBinaryRecord request, ServerCallContext context)
+        {
+            return Task.FromResult(new PlayerRecordValidationResult()
+            {
+                IsSuccess = true,
+                Key = request.Key,
+                UserId = request.UserId
+            });
+        }
+
+        public override Task<BulkPlayerRecordValidationResult> AfterBulkReadPlayerBinaryRecord(BulkPlayerBinaryRecord request, ServerCallContext context)
+        {
+            BulkPlayerRecordValidationResult result = new BulkPlayerRecordValidationResult();
+            foreach (var playerRecord in request.PlayerBinaryRecords)
             {
                 result.ValidationResults.Add(new PlayerRecordValidationResult()
                 {
